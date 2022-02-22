09a0c614d714b18a291ea35c953b66cd7d24bac4
Josiah Burnham <62119663+JosiahBurnham@users.noreply.github.com>
2022-02-22 18:14:25 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/bitmap.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/bitmap.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/bitmapCoreHeader.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/bitmapCoreHeader.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/bitmapFile.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/bitmapFile.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/bitmapFileHeader.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/bitmapFileHeader.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/bitmapInfoHeader.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/bitmapInfoHeader.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/bitmapReader.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/bitmapReader.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/bitmapWriter.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/bitmapWriter.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/encryptedBitmap.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/encryptedBitmap.cpp.obj"
+  "C:/Users/jjbur/Desktop/COP_3003_Test3/pixel.cpp" "C:/Users/jjbur/Desktop/COP_3003_Test3/cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/pixel.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/COP_3003_Test2.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COP_3003_Test2.dir/bitmap.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/bitmapCoreHeader.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/bitmapFile.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/bitmapFileHeader.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/bitmapInfoHeader.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/bitmapReader.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/bitmapWriter.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/encryptedBitmap.cpp.obj"
+  "CMakeFiles/COP_3003_Test2.dir/pixel.cpp.obj"
+  "COP_3003_Test2.exe"
+  "COP_3003_Test2.exe.manifest"
+  "COP_3003_Test2.pdb"
+  "libCOP_3003_Test2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COP_3003_Test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
